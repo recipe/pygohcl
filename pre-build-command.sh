5e926f9a2199aaa99740effa9e5aea35f318b501
Vitaliy Demidov <vitaliy@scalr.com>
2023-01-10 16:03:25 -0800

    fix GOOS

--- pre-build-command.sh
+++ pre-build-command.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env sh
 
+OS_NAME=$(uname)
+case $OS_NAME in
+  Darwin*)  OS="darwin" ;;
+  Linux*)   OS="linux" ;;
+  *)        echo "Unexpected OS: $OS_NAME"
+            exit 1
+            ;;
+esac
+
 ARCH=$(uname -m)
 case $ARCH in
     x86_64) ARCH="amd64" ;;
     aarch64) ARCH="arm64" ;;
 esac
-curl "https://storage.googleapis.com/golang/go1.13.linux-${ARCH}.tar.gz" --silent --location | tar -xz
+curl "https://storage.googleapis.com/golang/go1.13.${OS}-${ARCH}.tar.gz" --silent --location | tar -xz
 
 export PATH="$(pwd)/go/bin:$PATH"
