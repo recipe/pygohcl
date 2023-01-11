e6e63bf9378a3361408f111548976f66fed4abd0
Vitaliy Demidov <vitaliy@scalr.com>
2023-01-10 21:28:54 -0800

    ARCHFLAGS with proper go version

--- pre-build-command.sh
+++ pre-build-command.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
+set -e
+
 OS_NAME=$(uname)
 case $OS_NAME in
   Darwin*)  OS="darwin" ;;
@@ -9,17 +11,18 @@ case $OS_NAME in
             ;;
 esac
 
-case $ARCHFLAGS in
+if [ -z "$ARCHFLAGS" ]; then
+  ARCH=$(uname -m)
+else
+  ARCH=$ARCHFLAGS
+fi
+
+case $ARCH in
     *x86_64)  ARCH="amd64" ;;
-    *amd64)   ARCH="amd64" ;;
     *aarch64) ARCH="arm64" ;;
-    *arm64)   ARCH="arm64" ;;
-    *)        echo "Unexpected ARCH: $ARCHFLAGS"
-              exit 1
-              ;;
 esac
 
-curl "https://storage.googleapis.com/golang/go1.13.${OS}-${ARCH}.tar.gz" --silent --location | tar -xz
+curl "https://storage.googleapis.com/golang/go1.17.${OS}-${ARCH}.tar.gz" --silent --location | tar -xz
 export PATH="$(pwd)/go/bin:$PATH"
 
 echo "OS=$OS"
